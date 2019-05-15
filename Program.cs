18a930c2af3acc4bf36fc01f0b1d6d47ca0ffff5
Sergio Rodriguez <rsergio98@yahoo.com>
2019-05-15 10:28:02 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Testfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
